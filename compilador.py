9589eafb283a3246759f9c404adca838e6b3fa67
GeovaniK <geovani2k@yahoo.com.br>
2016-09-23 14:56:00 -0300

    Leitura dos tokens da gramatica

--- compilador.py
+++ compilador.py
@@ -6,21 +6,29 @@ tokens = []
 def leituraGramatica (arq):
 	lines = arq.readlines()#le tds as linhas da gramatica
 	
-	for line in lines:
-		print line
+	#for line in lines:
+		#print line
 
 	for line in lines:
+		k=1
 		partes = line.split("=")#separamos a linha em duas partes
+		producoes = partes[1].split("|")#separando as producoes na quantidade de procucoes
+		#print producoes
 		
-		producoes = partes[1].split("|")
+		for k in range(0,len(producoes)):
+			tokens = producoes[k][producoes[k].find(" ")+1:producoes[k].find("<")]
+			#k = k+1
+			print tokens
+		#tokens = producoes[0][producoes[0].find("<")+1:partes[0].find(">")]
 		
-		quantidade = len(producoes)
-		aux = 0
-		for aux in range(0,len(producoes)):
-			tokens[aux] = producoes[aux].split("<")
-			aux = aux + 1
+		#quantidade = len(producoes)
+		#aux = 0
+		#for aux in range(0,len(producoes)):
+		#	tokens[aux] = producoes[aux].split("<")
+		#	aux = aux + 1
 			
-		print producoes
+		#print ("\n")
+		
 
 
 
