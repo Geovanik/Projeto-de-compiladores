44b4abd9e2e0e551caa2e60e1004f6db265ddadd
LeonardoBelinski <leonardobelinski@yahoo.com.br>
2016-10-28 18:06:51 -0200

    Atualização preenche tabela

--- compilador.py
+++ compilador.py
@@ -1,6 +1,8 @@
 from sys import argv
 from sets import Set
+import csv
 matriz = []
+intermediaria = []
 tokens = []
 estados = Set([])
 indice = []
@@ -45,15 +47,19 @@ def leituraGramatica (arq):
 						except:
 							matriz[0][c] = []
 							indice.append(c)
+
+
 def preencheTabela (arq):
 	global matriz
 	global contEstado
 	global estadosFinais
 	criou = 0
+        teste_estado = 0;
 	lines = arq.readlines()#le tds as linhas da gramatica
 	matriz.append({})
 	for line in lines:
-		cont = 1
+		cont = 0
+                teste_estado = 0
 		partes = line.split("::=")#separamos a linha em duas partes
 		producoes = partes[1].split("|")#separando as producoes na quantidade de procucoes
 		for k in range(0,len(producoes)):#separando tokens
@@ -63,62 +69,88 @@ def preencheTabela (arq):
 				estado = producoes[k][producoes[k].find("<")+1:producoes[k].find(">")]
 			partes[0] = partes[0].replace("<", "")
 			partes[0] = partes[0].replace(">", "")
-			cont = 1
+			cont = 0
 			criou = 0
 			if "'" in producoes[k]:
+				print("PRODUCOES IF", producoes[k]);
 				tokens = producoes[k].replace("'", "")
 				tokens = tokens.replace(" ", "")
 				for c in tokens:
 					if c != "\n" and c != "\r":
+						if teste_estado == 0 :
+				        		contEstado+=1;                                    
+				    			aux = contEstado;
+                                        		teste_estado = 1;
 						try:
 							matriz[cont][c].append(contEstado)
-							if partes[0] != estado:
+							if estado == None:
 								cont=contEstado
-								contEstado+=1
 								criou = 1
 						except:
-							matriz.append({})
+							matriz.append({}) #talvez retirar
 							cont-=1
 							contEstado+=1
 							matriz[cont][c] = []
 							matriz[cont][c].append(contEstado)
 							cont=contEstado-1
-							
 							criou = 1
+				if producoes[k] == "''\n":
+				    cont=contEstado;
+				    criou = 1;
 			else:
-				tokens = tokens.replace("'", "")
+                                tokens = tokens.replace("'", "")
 				tokens = tokens.replace(" ", "")
-				for c in tokens:
-					if c != "\n" and c != "\r":
-						try:
-							matriz[cont][c].append(contEstado)
-							if partes[0] != estado:
-								cont=contEstado
-								contEstado+=1
+				if len(tokens) == 1:
+				    if teste_estado == 0 :
+				        contEstado+=1;                                    
+				    	aux = contEstado;
+                                        teste_estado = 1;
+				    try:
+					matriz[cont][tokens].append(aux)
+					if partes[0] != estado:
+					    cont=aux
+				            criou = 1;
+				    except:
+					matriz.append({}) #talvez retirar
+					if partes[0] != estado:
+					    cont=aux
+				            criou=1
+					matriz[cont][c] = []
+					matriz[cont][c].append(aux)
+		    	         	cont=aux;
+					criou = 1
+				else: 
+					tokens = tokens.replace("'", "")
+					tokens = tokens.replace(" ", "")
+					for c in tokens:
+						if c != "\n" and c != "\r":
+							try:
+								matriz[cont][c].append(contEstado+1)
+								if partes[0] != estado:
+									cont=contEstado
+									contEstado+=1
+									criou = 1
+								criou = 1;
+							except:
+								matriz.append({})
+								if partes[0] != estado:
+									cont=contEstado
+									contEstado+=1
+									criou=1
+								matriz[cont][c] = []
+								matriz[cont][c].append(contEstado)
+								cont=contEstado-1
 								criou = 1
-						except:
-							matriz.append({})
-							if partes[0] != estado:
-								contEstado+=1
-							matriz[cont][c] = []
-							matriz[cont][c].append(contEstado)
-							cont=contEstado-1
-							criou = 1
-				if estado == partes[0]:
-					criou = 0			
+					if estado == partes[0]:
+						criou = 0			
 			if criou:
 				matriz.append({})
-				matriz[contEstado]['*'] = []
-				matriz[contEstado]['*'] = '*'
 				estadosFinais.append(contEstado)
-				cont=contEstado-1
 				criou = 0
 		print
 	i = 0
-	b = 1
+	b = 0
 	a = 0
-	print "LEN MATRIZ ", len(matriz)
-	print "LEN Indice ", len(indice)
 	for a in range(0, len(matriz)):
 		i = 0
 		for k in range(0, len(indice)):
@@ -127,28 +159,82 @@ def preencheTabela (arq):
 			except:
 				pass			
 			i = i + 1
+			#print
 		b = b + 1
-		print "A ", a
 	print estadosFinais
+
+def detMat():
+	global contEstado
+	global matriz
+	global estadosFinais
+	global simbolos
+	global estadosAutomato
+	cont=0
+	existeInd=1
+	novoEstado = 0
+	while(existeInd):
+		if(cont > 100):
+			existeInd = 0
+		for c, v in matriz[cont].iteritems():
+			if len(v) > 1:
+				contEstado+=1
+				matriz.append({})
+				for i in range(0,len(v)):
+					pos=int(v[i])
+					for a in indice:
+						try:
+							if matriz[pos][a] is not None:
+								try:
+									matriz[contEstado][a] = matriz[contEstado][a] + matriz[pos][a]
+								except:
+									matriz[contEstado][a]=[]
+									matriz[contEstado][a] = matriz[contEstado][a] + matriz[pos][a]
+							
+						except:
+							pass
+						if pos in estadosFinais and contEstado not in estadosFinais:  
+							estadosFinais.append(contEstado)
+					novoEstado=1
+			if(novoEstado==1):
+				matriz[cont][c] = []
+				matriz[cont][c].append(contEstado)
+				print "Criou novo estado", contEstado, "indeterminismo em ", c, "com", v
+				novoEstado+=1
+				estados.add(contEstado)
+                print("CONT ", cont)
+		novoEstado=0		
+		cont+=1
+		if cont > 46:
+			existeInd=0
 		
 def imprime():
 	global matriz
 	a = ""#usado na impressao de valores 
-	#print (' '*11+'|'),
+	print (' '*9+'|'),
 	for simbolo in indice:#mostrou as ligacoes
-		print '%s' % (simbolo),"|",
+		print '%8s' % (simbolo),"|",
 		
 	print
-	print ('_ _'*(len(indice)*5))
-	for simbolo in indice:
-		try:
-			print '%s' % (matriz[0][simbolo]),
-		except:
-			print '%s' % (a),"X",
-	
-		print ('|'),
-	print
-	print
+	print ('_ _'*(len(indice)*3))
+	#verifique se o estado e final e imprima o *
+	for i in range(0,len(matriz)):
+		#if i not in inalcancaveis and i not in mortos:
+		if i in estadosFinais:
+			print "*",
+		else:
+			print " ",
+		print '%6s' % (i),"|",
+		#imprime os simbilos que sao nomes das regras
+			
+		for simbolo in indice:
+			try:
+				print '%8s' % (matriz[i][simbolo]),
+			except:
+				print '%6s' % (a),"X",
+				
+			print ('|'),
+		print
+		print
 
 
 ############################################################# MAIN
@@ -164,6 +250,126 @@ preencheTabela (arq)
 
 arq.close
 
-#print indice
+intermediaria = matriz;
 
-#imprime()
+i = 0;
+
+while i < contEstado:
+    for j in indice:
+        try:
+	    intermediaria[i][j] = intermediaria[i][j];
+	except:
+	    intermediaria[i][j] = []
+	    intermediaria[i][j].append("X")
+    i+=1;
+
+imprime()
+
+csvfile = "arq.csv"
+
+with open(csvfile, 'w') as csvfile:
+    writer = csv.writer(csvfile, dialect='excel')
+    writer.writerow(indice)
+    for i in range(0, len(intermediaria)):
+        if i < 46:
+		j = i;
+		char_i = intermediaria[j]['i'];
+		char_f = intermediaria[j]['f'];
+		char_e = intermediaria[j]['e'];
+		char_l = intermediaria[j]['l'];
+		char_s = intermediaria[j]['s'];
+		char_w = intermediaria[j]['w'];
+		char_h = intermediaria[j]['h'];
+		char_a = intermediaria[j]['a'];
+		char_o = intermediaria[j]['o'];
+		char_u = intermediaria[j]['u'];
+		char_1 = intermediaria[j]['1'];
+		char_2 = intermediaria[j]['2'];
+		char_3 = intermediaria[j]['3'];
+		char_4 = intermediaria[j]['4'];
+		char_5 = intermediaria[j]['5'];
+		char_6 = intermediaria[j]['6'];
+		char_7 = intermediaria[j]['7'];
+		char_8 = intermediaria[j]['8'];
+		char_9 = intermediaria[j]['9'];
+		char_0 = intermediaria[j]['0'];
+		char_ma = intermediaria[j]['+'];
+		char_me = intermediaria[j]['-'];
+		char_di = intermediaria[j]['/'];
+		char_di = intermediaria[j]['/'];
+		char_ve = intermediaria[j]['*'];
+		char_ig = intermediaria[j]['='];
+		char_mm = intermediaria[j]['>'];
+		char_mn = intermediaria[j]['<'];
+		char_m = intermediaria[j]['m'];
+		char_n = intermediaria[j]['n'];
+		char_r = intermediaria[j]['r'];
+		char_t = intermediaria[j]['t'];
+		char_b = intermediaria[j]['b'];
+		char_k = intermediaria[j]['k'];
+		char_c = intermediaria[j]['c'];
+		char_z = intermediaria[j]['z'];
+		writer.writerow([char_i,char_f,char_e,char_l,char_s,char_w,char_h,char_a,char_o,char_u,char_1,char_2,char_3,char_4,char_5,char_6,char_7,char_8,char_9,char_0,char_ma,char_me,char_di,char_ve,char_ig,char_mm,char_mn,char_m,char_n,char_r,char_t,char_b,char_k,char_c,char_z]);
+
+detMat()
+
+intermediaria = matriz
+
+i = 0;
+
+while i < contEstado:
+    for j in indice:
+        try:
+	    intermediaria[i][j] = intermediaria[i][j];
+	except:
+	    intermediaria[i][j] = []
+	    intermediaria[i][j].append("X")
+    i+=1;
+
+csvfile = "arqDet.csv";
+
+with open(csvfile, 'w') as csvfile:
+    writer = csv.writer(csvfile, dialect='excel')
+    writer.writerow(indice)
+    for i in range(0, len(intermediaria)):
+        if i < 49:
+		j = i;
+		char_i = intermediaria[j]['i'];
+		char_f = intermediaria[j]['f'];
+		char_e = intermediaria[j]['e'];
+		char_l = intermediaria[j]['l'];
+		char_s = intermediaria[j]['s'];
+		char_w = intermediaria[j]['w'];
+		char_h = intermediaria[j]['h'];
+		char_a = intermediaria[j]['a'];
+		char_o = intermediaria[j]['o'];
+		char_u = intermediaria[j]['u'];
+		char_1 = intermediaria[j]['1'];
+		char_2 = intermediaria[j]['2'];
+		char_3 = intermediaria[j]['3'];
+		char_4 = intermediaria[j]['4'];
+		char_5 = intermediaria[j]['5'];
+		char_6 = intermediaria[j]['6'];
+		char_7 = intermediaria[j]['7'];
+		char_8 = intermediaria[j]['8'];
+		char_9 = intermediaria[j]['9'];
+		char_0 = intermediaria[j]['0'];
+		char_ma = intermediaria[j]['+'];
+		char_me = intermediaria[j]['-'];
+		char_di = intermediaria[j]['/'];
+		char_di = intermediaria[j]['/'];
+		char_ve = intermediaria[j]['*'];
+		char_ig = intermediaria[j]['='];
+		char_mm = intermediaria[j]['>'];
+		char_mn = intermediaria[j]['<'];
+		char_m = intermediaria[j]['m'];
+		char_n = intermediaria[j]['n'];
+		char_r = intermediaria[j]['r'];
+		char_t = intermediaria[j]['t'];
+		char_b = intermediaria[j]['b'];
+		char_k = intermediaria[j]['k'];
+		char_c = intermediaria[j]['c'];
+		char_z = intermediaria[j]['z'];
+		writer.writerow([char_i,char_f,char_e,char_l,char_s,char_w,char_h,char_a,char_o,char_u,char_1,char_2,char_3,char_4,char_5,char_6,char_7,char_8,char_9,char_0,char_ma,char_me,char_di,char_ve,char_ig,char_mm,char_mn,char_m,char_n,char_r,char_t,char_b,char_k,char_c,char_z]);
+
+imprime()
