d17c67f1c45e41cf75c0d52754d71aa083b6d886
LeonardoBelinski <leonardobelinski@yahoo.com.br>
2016-12-18 16:17:24 -0200

    Alteração tratamento erros

--- compilador.py
+++ compilador.py
@@ -8,6 +8,161 @@ estados = Set([])
 indice = []
 contEstado = 1#contador de estados
 estadosFinais = []
+fita = []
+
+class Tab_simbolos_nula():
+	rotulo = "nula"
+	nome = "nula"
+	linha = "nula"
+
+class Tab_simbolos_erro():
+	rotulo = "erro"
+	nome = "erro"
+	linha = "erro"
+	
+class Tab_simbolos_3():
+	rotulo = "condicao"
+	nome = "if"
+	linha = 0 #sera preenchida quando for lido no codigo fonte do programa do usuario
+	
+class Tab_simbolos_7():
+	rotulo = "condicao"
+	nome = "else"
+	linha = 0
+	
+class Tab_simbolos_12():
+	rotulo = "laco"
+	nome = "while"
+	linha = 0
+	
+class Tab_simbolos_13():
+	rotulo = "variavel"
+	nome = "nome"
+	linha = 0
+	
+class Tab_simbolos_14():
+	rotulo = "numero"
+	nome = "nome"
+	linha = 0
+	
+class Tab_simbolos_15():
+	rotulo = "op_aritmetica"
+	nome = "nome"
+	linha = 0	
+
+class Tab_simbolos_17():
+	rotulo = "op_relacionais"
+	nome = "nome"
+	linha = 0
+	
+class Tab_simbolos_21():
+	rotulo = "reservado"
+	nome = "main"
+	linha = 0
+	
+class Tab_simbolos_27():
+	rotulo = "reservado"
+	nome = "return"
+	linha = 0
+	
+class Tab_simbolos_32():
+	rotulo = "reservado"
+	nome = "break"
+	linha = 0
+	
+class Tab_simbolos_40():
+	rotulo = "reservado"
+	nome = "continue"
+	linha	= 0
+
+class Tab_simbolos_46():
+	rotulo = "reservado"
+	nome = "sizeof"
+	linha = 0
+
+class Tab_simbolos_47():
+	rotulo = "op_relacionais"
+	nome = "igual"
+	linha = 0
+
+lista = []
+
+estado0 = Tab_simbolos_nula#apenas para as posicoes vazias
+estado1 = Tab_simbolos_erro
+estado3 = Tab_simbolos_3
+estado7 = Tab_simbolos_7
+estado12 = Tab_simbolos_12
+estado13 = Tab_simbolos_13
+estado14 = Tab_simbolos_14
+estado15 = Tab_simbolos_15
+estado17 = Tab_simbolos_17
+estado21 = Tab_simbolos_21
+estado27 = Tab_simbolos_27
+estado32 = Tab_simbolos_32
+estado40 = Tab_simbolos_40
+estado46 = Tab_simbolos_46
+estado47 = Tab_simbolos_47
+
+lista.append(estado0)#posicao 0
+lista.append(estado1)#posicao 1 ERRO
+lista.append(estado0)#posicao 2
+lista.append(estado3)#estado correto com suas informacoes
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado7)#estado correto com suas informacoes
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado12)#estado correto com suas informacoes
+lista.append(estado13)#estado correto com suas informacoes
+lista.append(estado14)#estado correto com suas informacoes
+lista.append(estado15)#estado correto com suas informacoes
+lista.append(estado0)#posicao 0
+lista.append(estado17)#estado correto com suas informacoes
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado21)#estado correto com suas informacoes
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado27)#estado correto com suas informacoes
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado32)#estado correto com suas informacoes
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado40)#estado correto com suas informacoes
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+lista.append(estado46)#estado correto com suas informacoes
+lista.append(estado47)#estado correto com suas informacoes
+lista.append(estado0)#posicao 0
+lista.append(estado0)#posicao 0
+
+
+print (lista[0].rotulo)
+print (lista[7].rotulo)
+print (lista[12].rotulo)
+print (lista[17].rotulo)
+print (lista[46].rotulo)
+#print (lista[46].linha)
+# estado - posicao na lista
+# 3 - 0
 
 def leituraGramatica (arq):
 	global matriz
@@ -21,7 +176,6 @@ def leituraGramatica (arq):
 		estado = partes[0].replace("<", "")
 		estado = estado.replace(">", "")
 		estados.add(estado)
-		
 		for k in range(0,len(producoes)):#separando tokens
 			tokens = producoes[k][producoes[k].find(" ")+1:producoes[k].find("<")]
 			tokens = tokens.replace("'", "")
@@ -72,7 +226,6 @@ def preencheTabela (arq):
 			cont = 0
 			criou = 0
 			if "'" in producoes[k]:
-				print("PRODUCOES IF", producoes[k]);
 				tokens = producoes[k].replace("'", "")
 				tokens = tokens.replace(" ", "")
 				for c in tokens:
@@ -87,13 +240,13 @@ def preencheTabela (arq):
 								cont=contEstado
 								criou = 1
 						except:
-							#matriz.append({}) #talvez retirar
+							matriz.append({}) #talvez retirar
 							cont-=1
 							contEstado+=1
 							matriz[cont][c] = []
 							matriz[cont][c].append(contEstado)
 							cont=contEstado-1
-							#criou = 1
+							criou = 1
 				if producoes[k] == "''\n":
 				    cont=contEstado;
 				    criou = 1;
@@ -118,7 +271,25 @@ def preencheTabela (arq):
 					matriz[cont][c] = []
 					matriz[cont][c].append(aux)
 		    	         	cont=aux;
-					#criou = 1
+				    if partes[0] == estado:
+                                        for j in range(0,len(producoes)):#separando tokens
+					    #estado = None
+					    tokens = producoes[j][producoes[j].find(" ")+1:(producoes[j].find("<"))]
+                                            tokens = tokens.replace(" ", "");
+                                            try:
+                                                if contEstado not in matriz[contEstado][tokens]:
+				                    matriz[contEstado][tokens].append(contEstado)
+					        if partes[0] != estado:
+					            cont=aux
+				                    criou = 1;
+				            except:
+					        #matriz.append({}) #talvez retirar
+                                                matriz.append({})
+					        if partes[0] != estado:
+					            cont=aux
+				                    criou=1
+					        matriz[contEstado][tokens] = []
+					        matriz[contEstado][tokens].append(contEstado);
 				else: 
 					tokens = tokens.replace("'", "")
 					tokens = tokens.replace(" ", "")
@@ -130,7 +301,7 @@ def preencheTabela (arq):
 									cont=contEstado
 									contEstado+=1
 									criou = 1
-								#criou = 1;
+								criou = 1;
 							except:
 								matriz.append({})
 								if partes[0] != estado:
@@ -140,12 +311,10 @@ def preencheTabela (arq):
 								matriz[cont][c] = []
 								matriz[cont][c].append(contEstado)
 								cont=contEstado-1
-								#criou = 1
+								criou = 1
 					if estado == partes[0]:
 						criou = 0			
 			if criou:
-                                print(producoes[k]);
-				print("TAM ", len(matriz));
 				matriz.append({})
 				estadosFinais.append(contEstado)
 				criou = 0
@@ -153,17 +322,17 @@ def preencheTabela (arq):
 	i = 0
 	b = 0
 	a = 0
-	for a in range(0, len(matriz)):
-		i = 0
-		for k in range(0, len(indice)):
-			try:
-				print matriz[b][indice[i]], "AUX " + indice[i], "B ", b
-			except:
-				pass			
-			i = i + 1
-			#print
-		b = b + 1
-	print estadosFinais
+	#for a in range(0, len(matriz)):
+	#	i = 0
+	#	for k in range(0, len(indice)):
+	#		try:
+	#			print matriz[b][indice[i]], "AUX " + indice[i], "B ", b
+	#		except:
+	#			pass			
+	#		i = i + 1
+	#		#print
+	#	b = b + 1
+	#print estadosFinais
 
 def detMat():
 	global contEstado
@@ -200,22 +369,20 @@ def detMat():
 			if(novoEstado==1):
 				matriz[cont][c] = []
 				matriz[cont][c].append(contEstado)
-				print "Criou novo estado", contEstado, "indeterminismo em ", c, "com", v
 				novoEstado+=1
 				estados.add(contEstado)
-                print("CONT ", cont)
 		novoEstado=0		
 		cont+=1
 		if cont > 46:
 			existeInd=0
 
-def verificaInt(lista):
+def verificaInt(lista,num,char):
     pos = []
     for aux in lista:
         try:
             pos.append(int(aux));
         except:
-            pass #estado final
+            pass
     return pos
 
 def leituraFonte():
@@ -246,22 +413,28 @@ def leituraFonte():
                                 pos = verificaInt(matriz[pos_aux][c],pos_aux,c);
                                 pos_aux = pos[0];
 		            except: #erro escrita
-                                gravaFita(1, cont_linhas, aux_linha[cont_componentes]);
-                                print("Nao existe ", matriz[pos_aux][c]);
                                 while c != " ":
                                     k+=1;
-                                    c = line[k];
-                                    print("K ", k);
-                                #return False
+                                    try:
+                                        c = line[k];
+                                    except:
+                                        break;
+                                k+=1;
+                                pos_aux = 1;
 	            else: #chamar a funcao que grava na fita
                         #estado 1 de erro
-                        gravaFita(pos_aux, cont_linhas, aux_linha[cont_split_linha]);                      
-			pos_aux = 0;
-                        cont_split_linha+=1
+                        if aux_linha[cont_componentes] != "": #espaco fim da linha
+                            if pos_aux not in estadosFinais:
+                                pos_aux = 1;
+                            gravaFita(pos_aux, cont_linhas, aux_linha[cont_componentes]);
+                            cont_componentes+=1                     
+			    pos_aux = 0;
+                            cont_split_linha+=1
                     cont+=1
-                    cont_componentes+=1
-            gravaFita(pos_aux, cont_linhas, aux_linha[cont_split_linha]);
-                                
+            if pos_aux not in estadosFinais:
+                pos_aux = 1;
+            if aux_linha[cont_componentes] != "" and pos_aux != 0: #espaco fim da linha
+                gravaFita(pos_aux, cont_linhas, aux_linha[cont_componentes]); 
     arq.close()
 
 def gravaFita(num_estado, linha, palavra):
@@ -271,7 +444,8 @@ def gravaFita(num_estado, linha, palavra):
         str_fita = str(num_estado) + ";" + palavra + ";" + aux.rotulo + ";" + str(linha) + ";\n"
         arq.write(str_fita);
     arq.close();
-
+    
+		
 def imprime():
 	global matriz
 	a = ""#usado na impressao de valores 
@@ -301,7 +475,6 @@ def imprime():
 		print
 		print
 
-
 ############################################################# MAIN
 arq = open("gramatica","r")
 
@@ -322,62 +495,17 @@ i = 0;
 while i < contEstado:
     for j in indice:
         try:
-	    intermediaria[i][j] = intermediaria[i][j];
+            if "X" not in intermediaria[i][j]:
+	        intermediaria[i][j] = intermediaria[i][j];
 	except:
 	    intermediaria[i][j] = []
 	    intermediaria[i][j].append("X")
     i+=1;
 
-imprime()
-
-csvfile = "arq.csv"
-
-with open(csvfile, 'w') as csvfile:
-    writer = csv.writer(csvfile, dialect='excel')
-    writer.writerow(indice)
-    for i in range(0, len(intermediaria)):
-        if i < 46:
-		j = i;
-		char_i = intermediaria[j]['i'];
-		char_f = intermediaria[j]['f'];
-		char_e = intermediaria[j]['e'];
-		char_l = intermediaria[j]['l'];
-		char_s = intermediaria[j]['s'];
-		char_w = intermediaria[j]['w'];
-		char_h = intermediaria[j]['h'];
-		char_a = intermediaria[j]['a'];
-		char_o = intermediaria[j]['o'];
-		char_u = intermediaria[j]['u'];
-		char_1 = intermediaria[j]['1'];
-		char_2 = intermediaria[j]['2'];
-		char_3 = intermediaria[j]['3'];
-		char_4 = intermediaria[j]['4'];
-		char_5 = intermediaria[j]['5'];
-		char_6 = intermediaria[j]['6'];
-		char_7 = intermediaria[j]['7'];
-		char_8 = intermediaria[j]['8'];
-		char_9 = intermediaria[j]['9'];
-		char_0 = intermediaria[j]['0'];
-		char_ma = intermediaria[j]['+'];
-		char_me = intermediaria[j]['-'];
-		char_di = intermediaria[j]['/'];
-		char_di = intermediaria[j]['/'];
-		char_ve = intermediaria[j]['*'];
-		char_ig = intermediaria[j]['='];
-		char_mm = intermediaria[j]['>'];
-		char_mn = intermediaria[j]['<'];
-		char_m = intermediaria[j]['m'];
-		char_n = intermediaria[j]['n'];
-		char_r = intermediaria[j]['r'];
-		char_t = intermediaria[j]['t'];
-		char_b = intermediaria[j]['b'];
-		char_k = intermediaria[j]['k'];
-		char_c = intermediaria[j]['c'];
-		char_z = intermediaria[j]['z'];
-		writer.writerow([char_i,char_f,char_e,char_l,char_s,char_w,char_h,char_a,char_o,char_u,char_1,char_2,char_3,char_4,char_5,char_6,char_7,char_8,char_9,char_0,char_ma,char_me,char_di,char_ve,char_ig,char_mm,char_mn,char_m,char_n,char_r,char_t,char_b,char_k,char_c,char_z]);
-
 detMat()
 
+arq = open("fita_saida.txt","w")
+arq.close
 intermediaria = matriz
 
 i = 0;
@@ -385,58 +513,12 @@ i = 0;
 while i < contEstado:
     for j in indice:
         try:
-	    intermediaria[i][j] = intermediaria[i][j];
+            if "X" not in intermediaria[i][j]:
+	        intermediaria[i][j] = intermediaria[i][j];
 	except:
 	    intermediaria[i][j] = []
 	    intermediaria[i][j].append("X")
     i+=1;
 
-csvfile = "arqDet.csv";
-
-with open(csvfile, 'w') as csvfile:
-    writer = csv.writer(csvfile, dialect='excel')
-    writer.writerow(indice)
-    for i in range(0, len(intermediaria)):
-        if i < 49:
-		j = i;
-		char_i = intermediaria[j]['i'];
-		char_f = intermediaria[j]['f'];
-		char_e = intermediaria[j]['e'];
-		char_l = intermediaria[j]['l'];
-		char_s = intermediaria[j]['s'];
-		char_w = intermediaria[j]['w'];
-		char_h = intermediaria[j]['h'];
-		char_a = intermediaria[j]['a'];
-		char_o = intermediaria[j]['o'];
-		char_u = intermediaria[j]['u'];
-		char_1 = intermediaria[j]['1'];
-		char_2 = intermediaria[j]['2'];
-		char_3 = intermediaria[j]['3'];
-		char_4 = intermediaria[j]['4'];
-		char_5 = intermediaria[j]['5'];
-		char_6 = intermediaria[j]['6'];
-		char_7 = intermediaria[j]['7'];
-		char_8 = intermediaria[j]['8'];
-		char_9 = intermediaria[j]['9'];
-		char_0 = intermediaria[j]['0'];
-		char_ma = intermediaria[j]['+'];
-		char_me = intermediaria[j]['-'];
-		char_di = intermediaria[j]['/'];
-		char_di = intermediaria[j]['/'];
-		char_ve = intermediaria[j]['*'];
-		char_ig = intermediaria[j]['='];
-		char_mm = intermediaria[j]['>'];
-		char_mn = intermediaria[j]['<'];
-		char_m = intermediaria[j]['m'];
-		char_n = intermediaria[j]['n'];
-		char_r = intermediaria[j]['r'];
-		char_t = intermediaria[j]['t'];
-		char_b = intermediaria[j]['b'];
-		char_k = intermediaria[j]['k'];
-		char_c = intermediaria[j]['c'];
-		char_z = intermediaria[j]['z'];
-		writer.writerow([char_i,char_f,char_e,char_l,char_s,char_w,char_h,char_a,char_o,char_u,char_1,char_2,char_3,char_4,char_5,char_6,char_7,char_8,char_9,char_0,char_ma,char_me,char_di,char_ve,char_ig,char_mm,char_mn,char_m,char_n,char_r,char_t,char_b,char_k,char_c,char_z]);
-
-imprime()
-
-print(estadosFinais);
+leituraFonte()
+print("ESTADO FINAIS ", estadosFinais);
